ba44683fc2b6c567f3053025475c9efbb142921e
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 12:24:59 +0530

    third file

---
+++ 01_Assigment/03_program.cpp
@@ -0,0 +1,13 @@
+// add two number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter value of a::"<<endl;
+    cin>>a;
+     cout<<"enter value of b::"<<endl;
+    cin>>b;
+    int sum= a+b;
+    cout<<"sum of a and b is::"<<sum<<endl;
+    
+}
