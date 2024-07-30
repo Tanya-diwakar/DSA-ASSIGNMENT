00999e0dfaff904410b706bd9058419983cbaf08
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 20:09:43 +0530

    nine file

---
+++ 01_Assigment/09_program.cpp
@@ -0,0 +1,24 @@
+//find area and parameters of ract //
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the a::"<<endl;
+    cin>>a;
+     cout<<"enter the b::"<<endl;
+    cin>>b;
+    int area;
+     int pm;
+    area=(a*b);
+    cout<<"area of ractangle is::"<<area<<endl;
+
+    int num1,num2;
+
+    cout<<"enter the num1::"<<endl;
+    cin>>num1;
+    cout<<"enter the num2::"<<endl;
+    cin>>num2;
+    pm= 2*(num1+num2);
+    cout<<"perimeter of ractangle is::"<<pm<<endl;
+
+}
