ecbeac24f6d9d893dec9e2bc9c1f23fa90062a2a
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 19:47:53 +0530

    eight file

---
+++ 01_Assigment/08_program.cpp
@@ -0,0 +1,14 @@
+// multiply two floating pont  number//
+
+#include<iostream>
+using namespace std;
+int main(){
+    float num1,num2;
+    cout<<"enter num1::";
+    cin>>num1;
+     cout<<"enter num2::";
+    cin>>num2;
+    float multi;
+    multi=(num1*num2);
+    cout<<"multiple of floting two numbers::"<<multi<<endl;
+}
