e01098c56960a3265f8425a52a7ec0acb142c353
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 19:37:37 +0530

    seven file

---
+++ 01_Assigment/07_program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int integerType;
+    char charType;
+    float floatType;
+    double doubleType;
+
+   // find size of these using sizeof()//
+     cout<<"size of int::"<<sizeof(integerType)<<endl;
+     cout<<"size of char::"<<sizeof(charType)<<endl;
+     cout<<"size of float::"<<sizeof(floatType)<<endl;
+     cout<<"size of double::"<<sizeof(doubleType)<<endl;
+     return 0;
+}
