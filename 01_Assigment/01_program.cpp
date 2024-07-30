3d236203b66f25c8799568ede20e4071a67fc858
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 12:00:25 +0530

    first file

---
+++ 01_Assigment/01_program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num;
+    cout<<"enter a number";
+    cin>>num;
+
+    if(num>0){
+        cout<<" number is posstive "<<endl;
+    }
+    else{
+       cout<<" number is negative"<<endl;
+    }
+    
+  }
