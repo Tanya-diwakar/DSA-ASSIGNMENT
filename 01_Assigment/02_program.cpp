cfe4d989444a57b84de69bc06e1cd3c24fee31a7
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 12:06:06 +0530

    second file

---
+++ 01_Assigment/02_program.cpp
@@ -0,0 +1,15 @@
+//even or add//
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter a number::";
+    cin>>num;
+    if( num%2==0){
+        cout<<"number is even"<<endl;
+    }
+    else{
+        cout<<"number is odd"<<endl;
+    }
+}
