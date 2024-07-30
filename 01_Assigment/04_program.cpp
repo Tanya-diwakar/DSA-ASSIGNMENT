9af98ca2ab0ae5ad6c1572e6ec2d406b70b1f89d
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 12:32:50 +0530

    four file

---
+++ 01_Assigment/04_program.cpp
@@ -0,0 +1,15 @@
+//simple  intrest//
+#include<iostream>
+using namespace std;
+int main(){
+    int p,r,t;
+    cout<<"enter principle::";
+    cin>>p;
+    cout<<"enter rate::";
+    cin>>r;
+    cout<<"enter time::";
+    cin>>t;
+    int si= (p*r*t)/100;
+    cout<<"simple intrest is::"<<si<<endl;
+
+}
