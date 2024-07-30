ec6ca106276f31b0b53431b163ec3edde3934617
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-30 12:57:09 +0530

    five file

---
+++ 01_Assigment/05_program.cpp
@@ -0,0 +1,13 @@
+//ASCI VALUE OF a//
+#include<iostream>
+using namespace std;
+int main(){
+    char ch ;
+    cout<<"enter  charchter::";
+    cin>>ch;
+    cout<<"ASCI value of is:"<<ch<< "is: "<<int(ch)<<endl;
+    return 0;
+
+
+
+}
