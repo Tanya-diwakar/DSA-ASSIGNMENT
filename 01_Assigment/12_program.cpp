78fd9923ee62798569508452e06ac54173907a30
Tanya-diwakar <tdiwakar1292003@gmail.com>
2024-07-31 18:12:18 +0530

    12 file

---
+++ 01_Assigment/12_program.cpp
@@ -0,0 +1,10 @@
+//print 1 to 100 number but condtion ye hai ki 3 numbers ko jump karne 1 ke bad 3 jump //
+
+#include<iostream>
+using namespace std;
+int main(){
+   int i;
+   for(i=1; i<=100; i=i+3){
+        cout<<i<<endl;
+   }
+}
